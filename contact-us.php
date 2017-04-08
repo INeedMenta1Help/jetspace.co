ee90b543ff62f65dce9ab1370b777682b8f2a6d7
Sam <knutsen@jetspace.co>
2017-04-08 15:52:08 +0100

    Removed unclosed script tag, already included in scripts.php

--- contact-us.php
+++ contact-us.php
@@ -143,16 +143,5 @@
 
  <?php include 'footer.php'; ?>
  <?php include 'scripts.php'; ?>
-    $("#slideshow > div:gt(0)").hide();
-
-    setInterval(function() {
-      $('#slideshow > div:first')
-        .fadeOut(2000)
-        .next()
-        .fadeIn(2000)
-        .end()
-        .appendTo('#slideshow');
-    },  6000);
-  </script>
   </body>
 </html>
