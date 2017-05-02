bb8fb9d09a4150e81997176705180f1a150623ad
Demonstrandum <knutsen@jetspace.co>
2017-05-02 23:17:01 +0100

    Indentation error.

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 <nav class="nav">
   <div class="container">
     <ul>
-      <li><div><a href="/"                >Home       </a></div></li>
+      <li><div><a href="/"          >Home       </a></div></li>
       <li><div><a href="/websites"  >Websites   </a></div></li>
       <li><div><a href="/portfolio" >Portfolio  </a></div></li>
       <li><div><a href="/about"     >About      </a></div></li>
