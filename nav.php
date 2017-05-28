722ea00ee8716bfe1b54d7ab5a15eb35fb1f42ff
SamTwigg <twigg@jetspace.co>
2017-05-28 21:50:07 +0100

    Removed div

--- nav.php
+++ nav.php
@@ -1,5 +1,4 @@
 <nav class="nav">
-  <div class="container">
     <ul>
       <li><div><a href="/"          >Home       </a></div></li>
       <li><div><a href="/websites"  >Websites   </a></div></li>
@@ -7,5 +6,4 @@
       <li><div><a href="/about"     >About      </a></div></li>
       <li><div><a href="/contact-us">Contact Us </a></div></li>
     </ul>
-  </div>
 </nav>
