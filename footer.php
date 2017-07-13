fae9d3ec6729307365232e173c05bed4a327cfbb
clichetouche <twigg@jetspace.co>
2017-07-13 22:59:58 +0100

    Added contact number to footer

--- footer.php
+++ footer.php
@@ -5,6 +5,7 @@
 			<span>&copy; Copyright 2017, All rights reserved JetSpace LTD. Registered UK Limited Company.</span>
 			<br />
 			<span><a class="footLink" style="padding: 0; margin: 0;" href="/contact-us">business@jetspace.co</a></span>
+			<span><a class="footLink" style="padding: 0; margin: 0;" href="tel:03302230162">0330 223 0162</a></span>
 		</div>
         <div class="footRight">
             <a class="footLink" href="https://github.com/jetspace-LTD">GitHub</a>
