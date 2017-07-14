7c548d145e7ab81943db368875c2d195af93c094
Demonstrandum <knutsen@jetspace.co>
2017-07-14 23:34:28 +0000

    Added header info

--- payments/id.php
+++ payments/id.php
@@ -1,10 +1,49 @@
 <?php
   session_start();
 ?>
-
 <html>
   <head>
-    <link rel="stylesheet" href="buy.css">
+    <meta charset="utf-8" />
+    <title>Provide ID | JetSpace</title>
+    <meta name="description" content="Get a look at what we're all about and see our FAQ section" />
+    <meta property="og:url" content="https://jetspace.co/" />
+    <!-- Don't give file name if DirectoryIndex (leave blanl if normal page) -->
+    <meta property="og:image" content="https://JetSpace.co/assets/media/images/content/thumbnail.png" />
+    <meta property="og:image:width" content="1000" />
+    <!--Edit depending on resolution of image (keep w and h the same)-->
+    <meta property="og:image:height" content="1000" />
+    <!-- keep width and height -->
+    <base target="_parent" />
+    <meta property="og:description" content="Get a look at what we're all about and see our FAQ section" />
+    <!-- BASIC INFO[end] -->
+
+    <!--Local CSS[start]-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/mobile.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/master.css" />
+
+    <link rel="stylesheet" type="text/css" href="./buy.css" />
+    <!-- Local CSS[end] -->
+
+    <!--External css sources[start]-->
+    <link rel="stylesheet prefetch" href="https://daneden.github.io/animate.css/animate.min.css" />
+    <!--ANIMATIONS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" />
+    <!--ICONS-->
+    <!-- External css[end] -->
+
+    <!--local font css[start]-->
+    <link rel="stylesheet" type="text/css" href="../assets/fonts/font.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/fonts/logo.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/fonts/misc.css" />
+    <!--local font css[end]-->
+    <!--Retrive head tags[start] -->
+    <?php include '../head.php'; ?>
+    <!--Retrive head tags[end] -->
+
+    <!--External js[start]-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
+    <!-- External js[end] -->
   </head>
 
   <body>
@@ -22,12 +61,16 @@
        <h2>Special ID</h2>
        <form action="" method="POST" id="card-form" style="display: none;">
          <div class="form-row">
-           <label>CVC</label>
+           <label>Enter the special ID you were provided with</label>
            <input type="text" name="cusID" id="cusID" placeholder="cus_C1YF53Ygh9hDf" />
          </div>
-
          <button type="submit" name="submit-button">Submit</button>
        </form>
      </div>
+     <script>
+       $('.payment').css({
+         'top': 'calc(50% - ' + $('.payment').outerHeight() + 'px / 2)'
+       });
+     </script>
   </body>
 </html>
